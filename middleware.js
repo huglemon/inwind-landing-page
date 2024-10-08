f8d3dc5ee8a992b0aa15de06c0c02d80ca41175c
keepchen <keepchen2016@qq.com>
2024-10-08 11:14:00 +0800

    Enhance multilingual for seo

--- middleware.js
+++ middleware.js
@@ -1,12 +1,22 @@
+import { reportLanguage } from './lib/function/lang';
 import { locales } from './lib/i18n';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request) {
 	const { pathname } = request.nextUrl;
 
+	const lang = reportLanguage(pathname)
+	request.headers.set('x-pathname', pathname)
+	request.headers.set('x-language-directory', lang)
+	const response = NextResponse.next({
+		request: {
+			headers: request.headers,  //  put your headers here
+		},
+	});
+
 	const isExit = locales.some((locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`);
 
-	if (isExit) return;
+	if (isExit) return response;
 
 	request.nextUrl.pathname = `/`;
 	return Response.redirect(request.nextUrl);
